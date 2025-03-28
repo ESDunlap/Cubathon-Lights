fde54e25fc342a7744e62a15d83e444a9469dc82
ESDunlap <dunlape3@my.erau.edu>
2025-03-27 22:13:25 -0700

    Memory Cube Build Added

--- Cubathon Lights/Assets/Scripts/FollowPlayer.cs
+++ Cubathon Lights/Assets/Scripts/FollowPlayer.cs
@@ -4,8 +4,24 @@ public class FollowPlayer : MonoBehaviour
 {
     public Transform player;
     public Vector3 offset;
+    public Vector3 reverseOffset;
+    public bool reverse = false;
     void Update()
     {
-        transform.position = player.position + offset;
+        if (Input.GetKeyDown("q"))
+        {
+            transform.Rotate(0.0f, 180.0f, 0.0f);
+            reverse= !reverse;
+            FindObjectOfType<PlayerMovement>().sidewaysForce = -FindObjectOfType<PlayerMovement>().sidewaysForce;
+        }
+
+        if (reverse)
+        {
+            transform.position = player.position + reverseOffset;
+        }
+        else
+        {
+            transform.position = player.position + offset;
+        }
     }
 }
--- Cubathon Lights/Assets/Scripts/PlayerMovement.cs
+++ Cubathon Lights/Assets/Scripts/PlayerMovement.cs
@@ -20,7 +20,7 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
-        if (rb.position.y < -1)
+        if (rb.position.y < -1 || rb.position.y > 2)
         {
             FindObjectOfType<GameManager>().EndGame();
         }
